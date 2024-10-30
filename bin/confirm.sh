5ff6a539e833693972c60bae39cf094a245a82ac
Beau Tateyama <beau@prooftrading.com>
2024-10-30 18:02:50 -0400

    remove -e

--- bin/confirm.sh
+++ bin/confirm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Prompt to continue in command chain (e.g.  confirm "You really want to revert everything this?" && git reset --hard)
 
-read -r -p "$(echo -e \\e[33m\\e[1m${1:-Are you sure?}\\e[0m) [y/N] " response
+read -r -p "$(echo \\e[33m\\e[1m${1:-Are you sure?}\\e[0m) [y/N] " response
 #read -r -p "\e[33m${1:-Are you sure? [y/N]}\e[0m" response
 case $response in
 [yY][eE][sS]|[yY])
